e0e1f01b0fbf17058bc50dc34cb478b5fe3d41ab
jose <dajoseva@gmail.com>
2023-05-29 16:58:04 -0500

    ejercicio31

---
+++ ejercicio31.php
@@ -0,0 +1,25 @@
+<?php
+
+
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action="ejercicio31.php" method="post">
+
+        <input type="text" name="txtNombre">
+
+        <input type="submit" value="Enviar informacion">
+
+   </form>
+</body>
+</html>
